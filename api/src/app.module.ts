ba93897974b978af8596e7bc921b812ea3ef1741
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2021-06-06 16:35:29 +0800

    Added NestJS Config and PostGres SQL

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<string>('TYPEORM_TYPE') as 'postgres',
+        host: configService.get<string>('TYPEORM_HOST'),
+        port: configService.get<number>('TYPEORM_PORT'),
+        username: configService.get<string>('TYPEORM_USERNAME'),
+        password: configService.get<string>('TYPEORM_PASSWORD'),
+        database: configService.get<string>('TYPEORM_SCHEMA'),
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('TYPEORM_SYNCHRONIZE'),
+      })
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
-export class AppModule {}
+export default class AppModule {}

5b9f35df5366ee480247ef0609db8acc7738ce4f
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2021-06-06 14:13:30 +0800

    Added Swagger and Updated Main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .setTitle('TodoList API')
+    .setDescription('API for TodoList Application')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT;
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({
+      origin: [
+        'http:localhost:4200',
+      ],
+    });
+  }
+
+  await app.listen(port);
 }
 bootstrap();

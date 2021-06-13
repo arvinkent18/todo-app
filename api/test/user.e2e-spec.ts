bb864bd41189787374c9f8f44e551ae644c0a42f
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2021-06-13 23:57:49 +0800

    Added User E2E Initial

--- api/test/user.e2e-spec.ts
+++ api/test/user.e2e-spec.ts
@@ -1,24 +1,30 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import AppModule from './../src/app.module';
+import UserModule from '../src/user/user.module';
+import User from '../src/user/user.entity';
 
-describe('AppController (e2e)', () => {
+describe('UserController (e2e)', () => {
   let app: INestApplication;
+
+  const mockUserRepository = {};
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+      imports: [UserModule],
+    })
+      .overrideProvider(getRepositoryToken(User))
+      .useValue(mockUserRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/users (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
   });
 });

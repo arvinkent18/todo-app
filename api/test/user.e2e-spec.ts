09f2c0861c65529c826a2c4979113bef62e46bdc
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2021-06-14 00:21:28 +0800

    Added User Creation E2E

--- api/test/user.e2e-spec.ts
+++ api/test/user.e2e-spec.ts
@@ -8,7 +8,16 @@ import User from '../src/user/user.entity';
 describe('UserController (e2e)', () => {
   let app: INestApplication;
 
-  const mockUserRepository = {};
+  const mockCreateUser = {
+    email: 'arvinkent121816@gmail.com',
+    password: 'test-password',
+    name: 'Arvin Kent Lazaga',
+  }
+
+  const mockUserRepository = {
+    createUser: jest.fn().mockResolvedValue(mockCreateUser),
+  };
+
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -22,9 +31,18 @@ describe('UserController (e2e)', () => {
     await app.init();
   });
 
-  it('/users (GET)', () => {
+  it('/user/register (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
+      .post('/user/register')
+      .send(mockCreateUser)
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .then(response => {
+        expect(response.body).toEqual({
+          email: mockCreateUser.email,
+          password: mockCreateUser.password,
+          name: mockCreateUser.name,
+        })
+      })
   });
 });
